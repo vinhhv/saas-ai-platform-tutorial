193229c5a82cbc4f76b474af5020433de37a59fa
Vinh Vu <vinhhv@uci.edu>
2024-05-18 12:16:30 -0700

    Add code generation

---
+++ app/api/code/route.ts
@@ -0,0 +1,43 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const instructionMessage: OpenAI.ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "You are a code generator. Your code reply should be in markdown code snippets. Please add an explanation of your work following the code snippet.",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse("OpenAI API Key not configured", { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages],
+    });
+
+    return NextResponse.json(response.choices[0].message);
+  } catch (error) {
+    console.log("[CODE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
