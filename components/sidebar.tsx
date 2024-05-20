6e60455ea2a181b8ec96e7ba8961de317b042618
Vinh Vu <vinhhv@uci.edu>
2024-05-19 22:01:16 -0700

    Add free trial counter component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,6 +14,7 @@ import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FreeCounter } from "./free-counter";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -64,7 +65,11 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
@@ -98,6 +103,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
